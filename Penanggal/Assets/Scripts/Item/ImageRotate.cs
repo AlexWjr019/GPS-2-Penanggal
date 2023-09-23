7e6db639f6e4b8a75cf0ebd185945e456d59c076
LilFeeder <0135023@student.uow.edu.my>
2023-09-23 18:13:04 +0800

    5

---
+++ Penanggal/Assets/Scripts/Item/ImageRotate.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ImageRotate : MonoBehaviour
+{
+    [SerializeField] private InputAction pressed, axis;
+
+    [SerializeField] private float rotateSpeed = 1.0f;
+
+    private Vector2 rotation;
+
+    private bool rotateAllowed;
+
+    private void Awake()
+    {
+        pressed.Enable();
+        axis.Enable();
+        pressed.performed += _ => { StartCoroutine(Rotate()); };
+        pressed.canceled += _ => { rotateAllowed = false; };
+        axis.performed += context => { rotation = context.ReadValue<Vector2>(); };
+    }
+
+    private IEnumerator Rotate()
+    {
+        rotateAllowed = true;
+        while (rotateAllowed)
+        {
+            //apply rotation
+            rotation *= rotateSpeed;
+            transform.Rotate(Vector3.up, rotation.x, Space.World);
+            transform.Rotate(Vector3.right, rotation.y, Space.World);
+            yield return null;
+        }
+    }
+
+    public void ResetImageRotation()
+    {
+        transform.rotation = Quaternion.identity;
+    }
+
+}
