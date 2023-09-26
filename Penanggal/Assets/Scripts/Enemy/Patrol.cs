589f22e522d29b07804bcf45c11a3c0ad97c30c4
AlexWjr019 <wjr26092005@gmail.com>
2023-09-26 17:35:52 +0800

    10
    Patrol

---
+++ Penanggal/Assets/Scripts/Enemy/Patrol.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Patrol : MonoBehaviour
+{
+    public List<Transform> points = new List<Transform>();
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        //agent.autoBraking = false;
+
+        GotoNextPoint();
+    }
+
+    void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            GotoNextPoint();
+        }
+    }
+
+    void GotoNextPoint()
+    {
+        if (points.Count == 0)
+        {
+            return;
+        }
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Count;
+    }
+}
