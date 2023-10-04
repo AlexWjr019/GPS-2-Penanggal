463c308b1b4d69532c743d9ed787e8d16962eebe
ChongSiuHin <107177861+ChongSiuHin@users.noreply.github.com>
2023-10-04 18:10:13 +0800

    interaction with object no showing button

--- Penanggal/Assets/Scripts/Player/InteractionSystem.cs
+++ Penanggal/Assets/Scripts/Player/InteractionSystem.cs
@@ -9,7 +9,7 @@ public class InteractionSystem : MonoBehaviour
     public LayerMask interactableLayer;
     public GameObject interactionUI;
     private GameObject currentInteractable;
-    public InventoryItems inventoryItemPrefab; // 引用你的 InventoryItems Prefab
+    public InventoryItems inventoryItemPrefab;
 
     private float raycastDistance = 3f;
 
@@ -29,9 +29,6 @@ public class InteractionSystem : MonoBehaviour
 
             if (hitObject != currentInteractable)
             {
-                HideInteractionUI();
-
-                ShowInteractionUI(hitObject);
 
                 currentInteractable = hitObject;
             }
@@ -42,33 +39,28 @@ public class InteractionSystem : MonoBehaviour
         }
         else
         {
-            HideInteractionUI();
             currentInteractable = null;
-        }
-    }
-
-    private void ShowInteractionUI(GameObject interactableObject)
-    {
-        if (interactionUI != null)
-        {
-            interactionUI.SetActive(true);
-        }
-    }
-
-    private void HideInteractionUI()
-    {
-        if (interactionUI != null)
-        {
-            interactionUI.SetActive(false);
         }
     }
 
     private void CheckForButtonClick()
     {
-        if (Input.GetButtonDown("Button1"))
+        if (Input.touchCount > 0)
         {
-            Debug.Log("Button1 clicked!");
-            InteractWithCurrentObject();
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, raycastDistance, interactableLayer))
+                {
+                    currentInteractable = hit.collider.gameObject;
+
+                    InteractWithCurrentObject();
+                }
+            }
         }
     }
 
@@ -77,22 +69,16 @@ public class InteractionSystem : MonoBehaviour
     {
         if (currentInteractable != null)
         {
-            // 如果有空的 InventorySlot，生成 InventoryItems Prefab 并放入其中
             InventorySlot emptySlot = InventoryManager.Instance.GetEmptySlot();
             if (emptySlot != null)
             {
-                // 创建 InventoryItems Prefab 并放入 InventorySlot 的子对象
                 InventoryItems newInventoryItem = Instantiate(inventoryItemPrefab, emptySlot.transform);
                 newInventoryItem.transform.localPosition = Vector3.zero;
                 newInventoryItem.transform.localScale = Vector3.one;
-                newInventoryItem.name = "InventoryItem"; // 设置名称
-                newInventoryItem.gameObject.SetActive(true); // 激活物体
+                newInventoryItem.name = "InventoryItem";
+                newInventoryItem.gameObject.SetActive(true);
 
-                // 执行 Interactable 对象的交互行为
                 currentInteractable.SetActive(false);
-
-                // 隐藏 InteractionUI
-                HideInteractionUI();
             }
             else
             {
