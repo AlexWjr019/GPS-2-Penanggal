8e484a860adcf3506fc8af15e228ff45f2aff80a
owokamii <us3r424815@gmail.com>
2023-10-27 07:03:56 +0800

    81
    Nursery cabinet open close animation done

---
+++ Penanggal/Assets/Scripts/Yvonne/TestingPlayAnimation.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class TestingPlayAnimation : MonoBehaviour
+{
+    public Animator cabinetAnimator;
+
+    public bool isOpen = false;
+
+    void Update()
+    {
+        /*if(Input.GetMouseButtonDown(0))
+        {
+            if(gameObject.CompareTag("Interactable"))
+            {
+                if(!isOpen)
+                {
+                    // play open animation
+                    isOpen = true;
+                    cabinetAnimator.SetBool("isOpen", true);
+                }
+                else
+                {
+                    // play close animation
+                    isOpen = false;
+                    cabinetAnimator.SetBool("isOpen", false);
+                }
+            }
+        }*/
+
+        if (!isOpen)
+        {
+            // play open animation
+            //isOpen = true;
+            cabinetAnimator.SetBool("isOpen", false);
+        }
+        else
+        {
+            // play close animation
+            //isOpen = false;
+            cabinetAnimator.SetBool("isOpen", true);
+        }
+    }
+}
