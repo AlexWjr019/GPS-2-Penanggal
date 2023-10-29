8e4397051cdcdb4a59a4bbbcd3dc144578e7f8a4
owokamii <us3r424815@gmail.com>
2023-10-30 03:37:06 +0800

    102
    candle placements, baked lightmaps for all the rooms, implemented candle on off and swapping lightmaps

--- Penanggal/Assets/Scripts/Lighting/InteractCandle.cs
+++ Penanggal/Assets/Scripts/Lighting/InteractCandle.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class InteractCandle : MonoBehaviour
@@ -7,19 +8,72 @@ public class InteractCandle : MonoBehaviour
     public Light[] candleLights;
     public ParticleSystem[] particleSystems;
 
+    public Texture2D[] darkLightmapDir, darkLightmapColor;
+    public Texture2D[] brightLightmapDir, brightLightmapColor;
+
+    public LightmapData[] darkLightmap, brightLightmap;
+
+    void Start()
+    {
+        List<LightmapData> dlightmap = new List<LightmapData>();
+
+        for (int i = 0; i < darkLightmapDir.Length; i++)
+        {
+            LightmapData lmdata = new LightmapData();
+
+            lmdata.lightmapDir = darkLightmapDir[i];
+            lmdata.lightmapColor = darkLightmapColor[i];
+
+            dlightmap.Add(lmdata);
+        }
+
+        darkLightmap = dlightmap.ToArray();
+
+        List<LightmapData> blightmap = new List<LightmapData>();
+
+        for (int i = 0; i < brightLightmapDir.Length; i++)
+        {
+            LightmapData lmdata = new LightmapData();
+
+            lmdata.lightmapDir = brightLightmapDir[i];
+            lmdata.lightmapColor = brightLightmapColor[i];
+
+            blightmap.Add(lmdata);
+        }
+
+        brightLightmap = blightmap.ToArray();
+    }
+
     void Update()
     {
-        if (candles[0] && !isOn[0])
+        /*if (candles[0] && !isOn[0])
         {
             candleLights[0].enabled = false;
-            particleSystems[0].Play();
+            particleSystems[0].Stop();
             //cabinetAnimator.SetBool("isOpen", false);
         }
         else
         {
             candleLights[0].enabled = true;
-            particleSystems[0].Stop();
+            particleSystems[0].Play();
             //cabinetAnimator.SetBool("isOpen", true);
+        }*/
+
+        if (candles[0] && !isOn[0])
+        {
+            candleLights[0].enabled = false;
+            //particleSystems[0].Stop();
+            LightmapSettings.lightmaps = darkLightmap;
+            //isOn[0] = true;
+            Debug.Log("dark");
+        }
+        else
+        {
+            candleLights[0].enabled = true;
+            //particleSystems[0].Play();
+            LightmapSettings.lightmaps = brightLightmap;
+            //isOn[0] = false;
+            Debug.Log("bright");
         }
     }
 }
