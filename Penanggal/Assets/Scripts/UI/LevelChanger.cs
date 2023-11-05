c15de3f2a9ddc5beddeac3501064dbbd6c2b9a28
owokamii <us3r424815@gmail.com>
2023-11-06 06:16:27 +0800

    139
    Replaced some of the models and textures and added assets to scene

---
+++ Penanggal/Assets/Scripts/UI/LevelChanger.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+
+    private int levelToLoad;
+
+    public void FadeToNextLevel()
+    {
+        FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
