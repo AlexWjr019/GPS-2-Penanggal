584b14fd43a1df0a1a6d0048631961dc4655ad50
owokamii <us3r424815@gmail.com>
2023-11-08 04:28:54 +0800

    173
    level 2 setup still incomplete

--- Penanggal/Assets/Scripts/ScriptedEvents/ScriptedEvent_6.cs
+++ Penanggal/Assets/Scripts/ScriptedEvents/ScriptedEvent_6.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class ScriptedEvent_6 : MonoBehaviour
 {
     public AudioSource windowShatter;
+    public GameObject bedroomKey;
 
     public void PlayWindowShatter()
     {
         Debug.Log("Play window shatter");
         windowShatter.Play();
+        bedroomKey.SetActive(true);
     }
 }
