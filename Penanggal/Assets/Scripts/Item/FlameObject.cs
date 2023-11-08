816885b50c557c3dc7775bbc44da77fa404d171e
LilFeeder <0135023@student.uow.edu.my>
2023-11-08 12:01:11 +0800

    187
    -

--- Penanggal/Assets/Scripts/Item/FlameObject.cs
+++ Penanggal/Assets/Scripts/Item/FlameObject.cs
@@ -1,11 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.SearchService;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.ProBuilder.Shapes;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
