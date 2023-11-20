766614551a50f5bfb9e6672978bbb3cc49698606
owokamii <us3r424815@gmail.com>
2023-11-21 05:41:08 +0800

    182
    minor changes: lowered down camera speed, fps cap at 30 changed to 60, turned off sky manager (skybox moving)

--- Penanggal/Assets/Scripts/Sky/SkyManager.cs
+++ Penanggal/Assets/Scripts/Sky/SkyManager.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class SkyManager : MonoBehaviour
 {
-    public float skySpeed;
+    /*public float skySpeed;*/
 
-    void Update()
+    public int target = 60;
+
+    void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = target;
+    }
+
+    /*void Update()
     {
         RenderSettings.skybox.SetFloat("_Rotation", Time.time * skySpeed);
-    }
+    }*/
+
+
 }
