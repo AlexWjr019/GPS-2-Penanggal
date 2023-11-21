f1aebf2bae46a77486011f7ace0f4b010c3e6864
ChongSiuHin <107177861+ChongSiuHin@users.noreply.github.com>
2023-11-21 20:37:19 +0800

    190
    Make new restart for lv2, and add on game manager and UI to lv2

--- Penanggal/Assets/Scripts/Checkpoint/PositionManager.cs
+++ Penanggal/Assets/Scripts/Checkpoint/PositionManager.cs
@@ -10,7 +10,7 @@ public class PositionManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
+            //DontDestroyOnLoad(gameObject);
         }
         else
         {
---
+++ Penanggal/Assets/Scripts/UI/LoseScene2.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoseScene2 : MonoBehaviour
+{
+    public Button Restart;
+    public Button mainMenu;
+    public GameObject loseUI;
+
+    public Penanggal penanggal;
+
+    private void Awake()
+    {
+        loseUI.SetActive(false);
+    }
+    void Start()
+    {
+        //FirstPersonController
+        //fps = FindObjectOfType<FirstPersonController>();
+        Restart.onClick.AddListener(RestartGame);
+        mainMenu.onClick.AddListener(GoToMainMenu);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void RestartGame()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
+        Time.timeScale = 1.0f;
+    }
+
+
+
+    public void GoToMainMenu()
+    {
+        Debug.Log("Trying to load main menu");
+        SceneManager.LoadScene("_MainMenu");
+        Time.timeScale = 1.0f;
+    }
+
+    public void PlayerCollidedWithGhost()
+    {
+        Time.timeScale = 0.0f;
+        loseUI.SetActive(true);
+    }
+
+}
